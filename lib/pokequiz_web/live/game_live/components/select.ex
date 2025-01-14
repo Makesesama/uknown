2279ad79b29e12a4f499845879f690d55bf61e0f
Makussu <maximilian@ruhm.info>
2025-01-14 20:09:16 +0100

    visuals

--- lib/pokequiz_web/live/game_live/components/select.ex
+++ lib/pokequiz_web/live/game_live/components/select.ex
@@ -4,11 +4,14 @@ defmodule PokequizWeb.GameLive.Lobby.SelectComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="">
-      <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 max-w-24 rounded" value="whois" phx-click="select_quiz">Who Is</button>
-      <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 max-w-24 rounded" value="weight_comparison" phx-click="select_quiz">Who is Heavier</button>
-      <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 max-w-24 rounded" value="pokemon_for_move" phx-click="select_quiz">Who can learn that Move</button>
-      <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 max-w-24 rounded" value="type_combination" phx-click="select_quiz">Type Compination Quiz</button>
+    <div class="flex flex-col min-h-screen justify-center items-center">
+      <h1 class="font-bold text-xl">Pick a game:</h1>
+      <div class="flex flex-col gap-2">
+        <button class="flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded justify-center" value="whois" phx-click="select_quiz"><p>Who Is</p></button>
+        <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded justify-center" value="weight_comparison" phx-click="select_quiz">Who is Heavier</button>
+        <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded justify-center" value="pokemon_for_move" phx-click="select_quiz">Who can learn that Move</button>
+        <button class="inline-flex flex items-center text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded justify-center" value="type_combination" phx-click="select_quiz">Type Compination Quiz</button>
+      </div>
     </div>
     """
   end
