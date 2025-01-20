7de5bac9a9250334c31a4f37b67dfba1841fa4e9
Makussu <maximilian@ruhm.info>
2025-01-20 21:19:34 +0100

    fix init callback

--- lib/pokequiz/quiz/core.ex
+++ lib/pokequiz/quiz/core.ex
@@ -1,6 +1,6 @@
 defmodule Pokequiz.Game do
 
-  @callback init() :: %{}
+  @callback init(socket :: %Phoenix.LiveView.Socket{}) :: %{}
 
   @callback display_name() :: String
   @callback value_handle() :: String
